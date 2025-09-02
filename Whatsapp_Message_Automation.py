d2d8f6dcb1a8e824060cb480546722c1fa0bb525
Puturu Ekambaram <mr.eka160604@gmail.com>
2025-09-02 17:02:45 +0530

    Add files via upload

---
+++ Whatsapp_Message_Automation.py
@@ -0,0 +1,4 @@
+#Whatsapp Message Automation
+import pywhatkit #Importing the pywhatkit library
+#Send message at a specific time
+pywhatkit.sendwhatmsg("+918978739021", "Hello! This is an automated message.", 16, 53)
